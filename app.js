1f0e7aee3bca420e19541d291c678a249e9b5070
Shadab-2604 <138616478+Shadab-2604@users.noreply.github.com>
2024-11-21 16:47:02 +0530

    Added vercel.json

--- app.js
+++ app.js
@@ -9,11 +9,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Create 'files' directory if it doesn't exist
+const filesDir = './files';
+if (!fs.existsSync(filesDir)) {
+    fs.mkdirSync(filesDir);
+}
+
 // Route for Home Page (List all files)
 app.get('/', (req, res) => {
-    fs.readdir('./files', (err, files) => {
+    fs.readdir(filesDir, (err, files) => {
         if (err) return res.status(500).send(err);
-        res.render('index', {files: files });
+        res.render('index', { files: files });
     });
 });
 
@@ -31,23 +37,22 @@ app.post('/create', (req, res) => {
     let count = 1;
 
     // Check if the file exists and update the file name
-    while (fs.existsSync(`./files/${fileName}`)) {
+    while (fs.existsSync(path.join(filesDir, fileName))) {
         fileName = `${fileNameBase}_(${count}).txt`;
         count++;
     }
 
     // Save data to file
-    fs.writeFile(`./files/${fileName}`, hisaabDetails, (err) => {
+    fs.writeFile(path.join(filesDir, fileName), hisaabDetails, (err) => {
         if (err) return res.status(500).send(err);
         res.redirect('/');
     });
 });
 
-
 // Route for editing a file
 app.get('/edit/:fileName', (req, res) => {
     const fileName = req.params.fileName;
-    fs.readFile(`./files/${fileName}`, 'utf8', (err, data) => {
+    fs.readFile(path.join(filesDir, fileName), 'utf8', (err, data) => {
         if (err) return res.status(500).send(err);
         res.render('edit', { fileName, data });
     });
@@ -55,32 +60,31 @@ app.get('/edit/:fileName', (req, res) => {
 
 // Route for updating the file after editing
 app.post('/update/:fileName', (req, res) => {
-    const {fileData} = req.body;
+    const { fileData } = req.body;
     const fileName = req.params.fileName;
-    fs.writeFile(`./files/${fileName}`, fileData, (err) => {
+    fs.writeFile(path.join(filesDir, fileName), fileData, (err) => {
         if (err) return res.status(500).send(err);
         res.redirect('/');
-    })
+    });
 });
 
 // Route for viewing a file
 app.get('/show/:fileName', (req, res) => {
-   const fileName = req.params.fileName;
-   fs.readFile(`./files/${fileName}`, 'utf8', (err, data) => {
-    if (err) return res.status(500).send(err);
-        res.render('show' , {fileName , data});
-   })
+    const fileName = req.params.fileName;
+    fs.readFile(path.join(filesDir, fileName), 'utf8', (err, data) => {
+        if (err) return res.status(500).send(err);
+        res.render('show', { fileName, data });
+    });
 });
 
 // Route for deleting a file
 app.get('/delete/:fileName', (req, res) => {
-   const fileName = req.params.fileName;
-   fs.unlink(`./files/${fileName}`, (err) => {
-    if (err) return res.status(500).send(err);
-    res.redirect('/');
-   })
+    const fileName = req.params.fileName;
+    fs.unlink(path.join(filesDir, fileName), (err) => {
+        if (err) return res.status(500).send(err);
+        res.redirect('/');
+    });
 });
-
 
 // Start server
 const port = process.env.PORT || 3000;
